aedb8023055c364722e48c4c2403b7e7a555c532
Nurayda Gumus <108728699+nuraydagumus@users.noreply.github.com>
2023-09-03 18:17:46 +0300

    Add files via upload

---
+++ Covid Deaths and Vacc Project - Data Exploration1.sql
@@ -0,0 +1,27 @@
+--
+-- File generated with SQLiteStudio v3.4.4 on Paz Eyl 3 18:17:17 2023
+--
+-- Text encoding used: windows-1258
+--
+PRAGMA foreign_keys = off;
+BEGIN TRANSACTION;
+
+-- Table: CovidDeaths
+CREATE TABLE CovidDeaths (iso_code, continent, location, date, population, total_cases, new_cases, new_cases_smoothed, total_deaths, new_deaths, new_deaths_smoothed, total_cases_per_million, new_cases_per_million, new_cases_smoothed_per_million, total_deaths_per_million, new_deaths_per_million, new_deaths_smoothed_per_million, reproduction_rate, icu_patients, icu_patients_per_million, hosp_patients, hosp_patients_per_million, weekly_icu_admissions, weekly_icu_admissions_per_million, weekly_hosp_admissions, weekly_hosp_admissions_per_million);
+
+-- Table: CovidVacc
+CREATE TABLE CovidVacc (iso_code, continent, location, date, total_tests, new_tests, total_tests_per_thousand, new_tests_per_thousand, new_tests_smoothed, new_tests_smoothed_per_thousand, positive_rate, tests_per_case, tests_units, total_vaccinations, people_vaccinated, people_fully_vaccinated, total_boosters, new_vaccinations, new_vaccinations_smoothed, total_vaccinations_per_hundred, people_vaccinated_per_hundred, people_fully_vaccinated_per_hundred, total_boosters_per_hundred, new_vaccinations_smoothed_per_million, new_people_vaccinated_smoothed, new_people_vaccinated_smoothed_per_hundred, stringency_index, population_density, median_age, aged_65_older, aged_70_older, gdp_per_capita, extreme_poverty, cardiovasc_death_rate, diabetes_prevalence, female_smokers, male_smokers, handwashing_facilities, hospital_beds_per_thousand, life_expectancy, human_development_index, excess_mortality_cumulative_absolute, excess_mortality_cumulative, excess_mortality, excess_mortality_cumulative_per_million);
+
+-- View: PercentPopulationVaccinated
+CREATE VIEW PercentPopulationVaccinated as
+Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations, SUM(cast(vac.new_vaccinations as bigint)) OVER (Partition by dea.location Order by
+dea.location, dea.date) as RollingPeopleVaccinated
+-- (RollingPeopleVaccinated/population)*100
+From CovidDeaths dea
+Join CovidVacc vac
+  On dea.location = vac.location
+  and dea.date = vac.date
+where dea.continent is not null;
+
+COMMIT TRANSACTION;
+PRAGMA foreign_keys = on;
